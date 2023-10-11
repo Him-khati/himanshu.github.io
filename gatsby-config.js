b227a1e0d90d5ed5a6acebd40d3f8c86bf7074ce
Himanshu <himanshu.temp@gig4ce.com>
2023-10-11 14:49:44 +0530

    Name Changed, site upgrade wip..

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Himanshu Khati',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Himanshu Khati is an experienced Android developer.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Himanshu Khati.</h2>;
+  const three = <h3 className="big-heading">I build android apps.</h3>;
   const four = (
     <>
       <p>
         I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://gigforce.in/" target="_blank" rel="noreferrer">
+          Gigforce
         </a>
         .
       </p>
