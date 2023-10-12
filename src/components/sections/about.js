f34e79c28d1bc184f9f4a0a2097593f0f08def02
Himanshu <himanshu.temp@gig4ce.com>
2023-10-12 21:39:00 +0530

    About me updated

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,33 +135,21 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hi! My name is Himanshu Khati. I am software engineer developing Android apps for more
+              than 6 Years. I am passionate about mobile and I'm always seeking to grow my skills
+              and work with teams who share the same passion.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Currently I am exploring <a href="https://flutter.dev/">Flutter</a>,{' '}
+              <a href="https://developer.apple.com/programs/">iOS</a>,{' '}
+              <a href="https://kotlinlang.org/docs/multiplatform.html">Kotlin Multiplatform</a>, and{' '}
+              <a href="https://developer.android.com/jetpack/compose">Jetpack Compose</a>. I am
+              currently working @<a href="https://gigforce.in/">gigforce</a> as Lead mobile
+              developer
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few technologies I’ve worked with:</p>
           </div>
 
           <ul className="skills-list">
